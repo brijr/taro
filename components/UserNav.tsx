5c91f4559d92390d24aa0be604b55c01ac52e369
Bridger Tower <bridgertower@gmail.com>
2024-10-01 15:27:59 -0600

    Updated

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -16,8 +16,8 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@username" />
-            <AvatarFallback>UN</AvatarFallback>
+            {/* <AvatarImage src="/avatars/01.png" alt="@username" /> */}
+            <AvatarFallback>LFG</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
