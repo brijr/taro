90e2ae6314153c40fd18141295e66f6a6bde9ca6
Bridger Tower <bridgertower@gmail.com>
2024-10-01 14:59:51 -0600

    Updated

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,21 +1,21 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import { client, db } from './drizzle';
+import dotenv from "dotenv";
+import path from "path";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { client, db } from "./drizzle";
 
 dotenv.config();
 
 async function main() {
-  console.log('Running migrations...');
+  console.log("Running migrations...");
   await migrate(db, {
-    migrationsFolder: path.join(process.cwd(), '/lib/db/migrations'),
+    migrationsFolder: path.join(process.cwd(), "/lib/db/migrations"),
   });
-  console.log('Migrations complete');
+  console.log("Migrations complete");
   await client.end();
 }
 
 main().catch((err) => {
-  console.error('Migration failed');
+  console.error("Migration failed");
   console.error(err);
   process.exit(1);
 });
