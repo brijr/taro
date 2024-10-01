1ce7492c235b99110b817a18f3c626272fc4fdb6
Bridger Tower <bridgertower@gmail.com>
2024-10-01 14:36:23 -0600

    Updated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+      {
+        source: "/openapi.yaml",
+        destination: "/api/openapi",
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
   },
 };
 
